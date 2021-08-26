f9d5d83a8980dea2f6e9cb50fd75943a72b5409a
Ackerman, Adam P <adamackrmn@gmail.com>
2021-08-26 16:14:06 -0500

    Finished the Loops problems for refresher

---
+++ 3013_Refresher/Loops1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Loops1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double sum = 0;
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine($"Please enter your score for Exam {i+1} >>");
+                sum += Convert.ToDouble(Console.ReadLine());
+                //sum = sum + ...
+            }
+
+            double average = sum / 3;
+            average /= 100;
+
+            Console.WriteLine($"Your exam average is {average.ToString("P4")}");
+
+        }
+    }
+}
---
+++ 3013_Refresher/Loops2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Loops2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double sum = 0;
+            double examCount = 0;
+
+            do
+            {
+                examCount++;
+                Console.WriteLine($"What is your score for exam {examCount}? please enter as a percent (e.g. 90% = 0.90) >>");
+                sum += Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Do you have another score to enter? yes or no >>");
+
+            } while (Console.ReadLine().ToLower() == "yes");
+
+            double avg = sum / examCount;
+
+            Console.WriteLine($"Your average is {avg.ToString("p")}");
+
+        }
+    }
+}
