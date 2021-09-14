f09e11f6fa651c55022222064ad4a7d372d6cb4b
Ackerman, Adam P <adamackrmn@gmail.com>
2021-09-14 15:24:42 -0500

    Completed the participation for vowels and consonants

---
+++ Participations/Vowels_and_Consonants/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Vowels_and_Consonants
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+        }
+
+        private void btnProcess_Click(object sender, RoutedEventArgs e)
+        {
+            lstConsonants.Items.Clear();
+            lstConsonants.Items.Clear();
+
+            string word = txtWord.Text;
+
+            foreach (char letter in word.ToLower())
+            {
+
+                if (letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u')
+                {
+                    lstVowels.Items.Add(letter);
+                }
+                else
+                {
+                    lstConsonants.Items.Add(letter);
+                }
+            }
+
+            txtWord.Clear();
+
+            //txtWord.Text = "";
+        }
+    }
+}
