72534b533f68070f996f16752c5abc7274ee0b16
Ackerman, Adam P <adamackrmn@gmail.com>
2021-09-21 15:26:59 -0500

    Completed participation over toy

--- Participations/WPF_Classes/MainWindow.xaml.cs
+++ Participations/WPF_Classes/MainWindow.xaml.cs
@@ -24,5 +24,60 @@ namespace WPF_Classes
         {
             InitializeComponent();
         }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            bool inputIsValid = true;
+
+            if (string.IsNullOrWhiteSpace(txtImage.Text) == true)
+            {
+                inputIsValid = false;
+                MessageBox.Show("Please input a valid response for image");
+            }
+
+            if (string.IsNullOrWhiteSpace(txtManufacturer.Text) == true)
+            {
+                inputIsValid = false;
+                MessageBox.Show("Please input a valid response for manufacturer");
+            }
+
+            if (string.IsNullOrWhiteSpace(txtName.Text) == true)
+            {
+                inputIsValid = false;
+                MessageBox.Show("Please input a valid response for name");
+            }
+
+            double price;
+
+            if (double.TryParse(txtPrice.Text, out price) == false)
+            {
+                inputIsValid = false;
+                MessageBox.Show("Please input a valid response for price");
+            }
+
+            if (inputIsValid == false)
+            {
+                return;
+            }
+
+            Toy t = new Toy();
+            t.Image = txtImage.Text;
+            t.Manufacturer = txtManufacturer.Text;
+            t.Name = txtName.Text;
+            t.Price = price;
+
+            lstToys.Items.Add(t);
+
+        }
+
+        private void lstToys_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            Toy selectedToy = (Toy)lstToys.SelectedItem;
+            MessageBox.Show(selectedToy.GetAisle());
+
+            var uri = new Uri(selectedToy.Image);
+            var img = new BitmapImage(uri);
+            imgToy.Source = img;
+        }
     }
 }
